4fb0b46cf0673a08debef8b3f5654d28247d72ed
vanbungkring <arie@wego.com>
2012-09-18 00:37:59 +0700

    adding function setParameters()

--- library/js/hammer.js
+++ library/js/hammer.js
@@ -1,7 +1,8 @@
 var global_api = 'd1d11c0cf2605c1e396a';
 $(document).ready(function() {
+	///////call anything u want
 });
 
-function setParameters() {
-	// adding function to set parameters date time, and  location of hotels
+function setParameters(a, b, c, d) {
+	// adding function to set parameters date time, and location of hotels
 }
