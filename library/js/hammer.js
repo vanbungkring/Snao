33f005a6efd2921f99b971d8b016e74a6330d2e7
ekampramudita <ekampramudita@gmail.com>
2012-09-19 10:31:04 +0700

    adding hitung ganti script

--- library/js/hammer.js
+++ library/js/hammer.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function() {
 	///////call anything u want
+	
+	//hitung gaji pertahun
+	function calc() {
+				$('#totalGaji').val((parseInt($('#gaji_bulan').val(), 10) + parseInt($("#gaji_lain").val(), 10) - parseInt($("#htg").val(), 10)) * 12);
+			}
+			$('#gaji_bulan').keyup(calc);
+			$("#gaji_lain").keyup(calc);
+			$("#htg").keyup(calc);
 });
